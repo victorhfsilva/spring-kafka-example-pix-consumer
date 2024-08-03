2bbde6172cc75a74347e77b2179e233d444ac707
Victor Hugo Ferreira Silva <victorhfsilva@protonmail.com>
2024-08-03 16:40:17 -0300

    feat: configure retryable topic

--- src/main/java/com/example/spring_kafka_example_pix_consumer/config/KafkaConfig.java
+++ src/main/java/com/example/spring_kafka_example_pix_consumer/config/KafkaConfig.java
@@ -2,14 +2,16 @@ package com.example.spring_kafka_example_pix_consumer.config;
 
 import com.example.spring_kafka_example_pix_consumer.model.dto.PixResponseDto;
 import com.example.spring_kafka_example_pix_consumer.util.JacksonDeserializer;
+import com.example.spring_kafka_example_pix_consumer.util.JacksonSerializer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.*;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import java.util.HashMap;
@@ -54,5 +56,25 @@ public class KafkaConfig {
         ConcurrentKafkaListenerContainerFactory<String, PixResponseDto> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         return factory;
+    }
+
+    @Bean
+    public ProducerFactory<String, PixResponseDto> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                bootstrapAddress);
+        configProps.put(
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class);
+        configProps.put(
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                JacksonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, PixResponseDto> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
     }
 }
--- src/main/java/com/example/spring_kafka_example_pix_consumer/service/impl/PixValidatorImpl.java
+++ src/main/java/com/example/spring_kafka_example_pix_consumer/service/impl/PixValidatorImpl.java
@@ -1,14 +1,14 @@
 package com.example.spring_kafka_example_pix_consumer.service.impl;
 
-import com.example.spring_kafka_example_pix_consumer.model.KeyTable;
 import com.example.spring_kafka_example_pix_consumer.model.Pix;
 import com.example.spring_kafka_example_pix_consumer.model.dto.PixResponseDto;
-import com.example.spring_kafka_example_pix_consumer.repository.KeyTableRepository;
 import com.example.spring_kafka_example_pix_consumer.repository.PixRepository;
 import com.example.spring_kafka_example_pix_consumer.service.KeyValidator;
 import com.example.spring_kafka_example_pix_consumer.service.PixValidator;
 import lombok.AllArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -25,6 +25,12 @@ public class PixValidatorImpl implements PixValidator {
 
     @Override
     @KafkaListener(topics = TOPIC, groupId = GROUP_ID)
+    @RetryableTopic(
+            backoff = @Backoff(value = 3000L),
+            attempts = "5",
+            autoCreateTopics = "true",
+            include = Exception.class
+    )
     public void validatePix(PixResponseDto pixResponseDto) {
         System.out.println("Received Pix: " + pixResponseDto.getIdentifier());
 
